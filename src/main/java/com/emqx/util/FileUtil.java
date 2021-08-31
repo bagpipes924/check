803121bf308bdaf92b07867f0cda8fe668c7a836
bagpipes@126.com <bagpipes@126.com>
2021-08-31 10:05:05 +0800

    test

---
+++ src/main/java/com/emqx/util/FileUtil.java
@@ -0,0 +1,9 @@
+package com.emqx.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class FileUtil {
+	private final static Logger logger = LoggerFactory.getLogger(FileUtil.class);
+}
